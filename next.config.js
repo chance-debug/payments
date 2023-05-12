f935b367585b0a1c052b221f0a897713f925b51a
chance-debug <bsc-13-18@cc.ac.mw>
2023-05-12 04:40:25 +0200

    OT-19 <message>

--- next.config.js
+++ next.config.js
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
+// const withTM = require('next-transpile-modules')( ['@square/web-sdk','react-square-web-payments-sdk'])
+
+// module.exports = withTM({
+//   env: {
+//     SQUARE_ACCESS_TOKEN: 'EAAAEQJoHO2cqAoQu8EfYLDQCw2iDqS0jbvsIl5KfV0Mcwex8LL-IW7uzu_2depr'
+//   },
+//   reactStrictMode: true,
+//   experimental: {
+//     esmExternals: 'loose'
+//   }
+// })
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  images: {
+		domains: ['images.unsplash.com'],
+	},
+  transpilePackages: ['@acme/ui', 'lodash-es'],
+};
+
+
 
 module.exports = nextConfig
---
+++ pages/api/payment.ts
@@ -0,0 +1,29 @@
+import { Client } from "square";
+import { randomUUID } from "crypto";
+
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
+const { paymentsApi } = new Client({
+  accessToken:
+    "EAAAEN5Vtdl5FqyfFIk7Hm04jQeu_g7J-S8EItzvVQi-MCGErQzSMwCmOlG1_fC6",
+  environment: "sandbox" as any,
+});
+
+export default async function handler(req: any, res: any) {
+  if (req.method === "POST") {
+    const { result } = await paymentsApi.createPayment({
+      idempotencyKey: randomUUID(),
+      sourceId: req.body.sourceId,
+      amountMoney: {
+        currency: "USD",
+        amount: req.body.payment_amount as any,
+      },
+    });
+    console.log(result);
+    res.status(200).json(result);
+  } else {
+    res.status(500).send();
+  }
+}
